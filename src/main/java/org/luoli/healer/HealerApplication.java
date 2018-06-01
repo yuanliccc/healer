8abbda3e1050cd2c1f816bf262b039e26cb2419c
YuanLicc <yl123456789lxp@163.com>
2018-06-01 22:28:07 +0800

    add mybatis and jdbc config

--- src/main/java/org/luoli/healer/HealerApplication.java
+++ src/main/java/org/luoli/healer/HealerApplication.java
@@ -2,13 +2,21 @@ package org.luoli.healer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class HealerApplication {
+public class HealerApplication  extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HealerApplication.class);
+    }
 
     public static void main(String[] args) {
+
         SpringApplication.run(HealerApplication.class, args);
     }
 }
--- src/main/java/org/luoli/healer/controller/SpringBootAPI.java
+++ src/main/java/org/luoli/healer/controller/SpringBootAPI.java
@@ -1,7 +1,6 @@
 package org.luoli.healer.controller;
 
 import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
